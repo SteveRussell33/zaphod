80a339de73036bd2f0c9f89a3b68558925fc0df4
Mike Jarmy <mjarmy@gmail.com>
2022-08-28 18:53:13 -0400

    redo layout

--- src/FM.cpp
+++ src/FM.cpp
@@ -3,25 +3,25 @@
 
 struct FM : Module {
     enum ParamId {
-        RATIO_PARAM,
-        //OFFSET_PARAM,
+        //RATIO_PARAM,
         PARAMS_LEN
     };
     enum InputId {
-        FM_INPUT,
+        //FM_INPUT,
         INPUTS_LEN
     };
     enum OutputId {
-        FM_OUTPUT,
+        //FM_OUTPUT,
         OUTPUTS_LEN
     };
 
     FM() {
         config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, 0);
-        configParam(RATIO_PARAM, 0.f, 1.f, 0.f, "Freq Ratio");
-        //configParam(OFFSET_PARAM, 0.f, 1.f, 0.f, "Freq Offset");
-        configInput(FM_INPUT, "V/Oct");
-        configOutput(FM_OUTPUT, "V/Oct");
+
+        //configParam(RATIO_PARAM, 0.f, 1.f, 0.f, "Freq Ratio");
+
+        //configInput(FM_INPUT, "V/Oct");
+        //configOutput(FM_OUTPUT, "V/Oct");
     }
 
     void process(const ProcessArgs& args) override {
@@ -34,14 +34,13 @@ struct FMWidget : ModuleWidget {
         setModule(module);
         setPanel(createPanel(asset::plugin(pluginInstance, "res/FM.svg")));
 
-        addChild(createWidget<ScrewSilver>(Vec(0, 0)));
-        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15, 365)));
+        //addChild(createWidget<ScrewSilver>(Vec(0, 0)));
+        //addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15, 365)));
 
-        addParam(createParamCentered<ZaphodKnob36>(Vec(30, 76), module, FM::RATIO_PARAM));
-        //addParam(createParamCentered<ZaphodKnob36>(Vec(30, 174), module, FM::OFFSET_PARAM));
+        //addParam(createParamCentered<ZaphodKnob36>(Vec(30, 76), module, FM::RATIO_PARAM));
 
-        addInput(createInputCentered<ZaphodPort24>(Vec(30, 266), module, FM::FM_INPUT));
-        addOutput(createOutputCentered<ZaphodPort24>(Vec(30, 310), module, FM::FM_OUTPUT));
+        //addInput(createInputCentered<ZaphodPort>(Vec(30, 266), module, FM::FM_INPUT));
+        //addOutput(createOutputCentered<ZaphodPort>(Vec(30, 310), module, FM::FM_OUTPUT));
     }
 };
 
