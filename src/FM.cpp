3e7e9f5567f9f5ec35b0400dcba4f028aca1cafc
Mike Jarmy <mjarmy@gmail.com>
2022-08-28 15:12:11 -0400

    stub out FM module

---
+++ src/FM.cpp
@@ -0,0 +1,44 @@
+#include "plugin.hpp"
+
+struct FM : Module {
+	//enum ParamId {
+	//	PITCH_PARAM,
+	//	PARAMS_LEN
+	//};
+	//enum InputId {
+	//	PITCH_INPUT,
+	//	INPUTS_LEN
+	//};
+	//enum OutputId {
+	//	SINE_OUTPUT,
+	//	OUTPUTS_LEN
+	//};
+	//enum LightId {
+	//	BLINK_LIGHT,
+	//	LIGHTS_LEN
+	//};
+
+	FM() {
+		//config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		//configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
+		//configInput(PITCH_INPUT, "");
+		//configOutput(SINE_OUTPUT, "");
+	}
+
+	void process(const ProcessArgs& args) override {
+	}
+};
+
+
+struct FMWidget : ModuleWidget {
+	FMWidget(FM* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/FM.svg")));
+
+        addChild(createWidget<ScrewSilver>(Vec(0, 0)));
+        addChild(createWidget<ScrewSilver>(Vec(box.size.x - 15, 365)));
+	}
+};
+
+
+Model* modelFM = createModel<FM, FMWidget>("FM");
